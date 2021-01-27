d0d6af527a715c7e60f8c1082046642c0603433a
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2021-01-27 14:09:00 -0600

    Modulos y funciones

---
+++ index.js
@@ -0,0 +1,11 @@
+const math = require('./maths.js');
+
+console.log("SUMA");
+console.log(math.add(10,2));
+console.log("RESTA");
+console.log(math.substract(10,2));
+console.log("DIVICION");
+console.log(math.divide(10,2));
+console.log("MULTIPLICACION");
+console.log(math.multiply(10,2));
+
