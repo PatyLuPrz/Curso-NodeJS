5196beb9bb7d8f60a351bf0f093005cdafdf668c
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2021-01-27 14:33:05 -0600

    Modulos y callbacks

--- index-modules.js
+++ index-modules.js
@@ -1,6 +1,17 @@
-const os = require('os');
+const fs = require('fs');
+// const os = require('os');
 
-console.log(os.platform());
-console.log(os.release());
-console.log("Free mem: ",os.freemem()," Bytes");
-console.log("Total mem: ",os.totalmem()," Bytes");
+// console.log(os.platform());
+// console.log(os.release());
+// console.log("Free mem: ",os.freemem()," Bytes");
+// console.log("Total mem: ",os.totalmem()," Bytes");
+
+fs.writeFile('./texto.txt',"linea uno",function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Archivo creado");
+    }
+});
+
+console.log("Ultima linea de codigo");
