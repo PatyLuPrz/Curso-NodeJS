b12aeaa7952d3282b7f5261e14cc4979d2bebc80
PatyLuPrz <1717110752@utectulancingo.edu.mx>
2021-01-27 14:40:04 -0600

    Crear y leer archivos

--- index-modules.js
+++ index-modules.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
-// const os = require('os');
+const os = require('os');
 
-// console.log(os.platform());
-// console.log(os.release());
-// console.log("Free mem: ",os.freemem()," Bytes");
-// console.log("Total mem: ",os.totalmem()," Bytes");
+// MODULO OS
+
+
+
+console.log(os.platform());
+console.log(os.release());
+console.log("Free mem: ",os.freemem()," Bytes");
+console.log("Total mem: ",os.totalmem()," Bytes");
+
+/// MODULO FS
 
 fs.writeFile('./texto.txt',"linea uno",function(err){
     if(err){
@@ -15,3 +21,11 @@ fs.writeFile('./texto.txt',"linea uno",function(err){
 });
 
 console.log("Ultima linea de codigo");
+
+fs.readFile('./texto.txt',function(err,data){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(data.toString());
+    }
+})
